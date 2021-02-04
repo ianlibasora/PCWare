48bdf40b57cb9f4120b823df17dfbf44eb9bfe62
libasoj2 <joseph.libasora2@mail.dcu.ie>
2021-02-04 16:26:26 +0000

    Added product + product category models

--- PCWare/PCStore/models.py
+++ PCWare/PCStore/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class ProductCategory(models.Model):
+    categoryID = models.AutoField(primary_key=True)
+    categoryName = models.CharField(max_length=100)
+    categoryDetails = models.CharField(max_length=200)
+
+
+class Product(models.Model):
+    productID = models.AutoField(primary_key=True)
+    productName = models.CharField(max_length=100)
+    productDetails = models.CharField(max_length=200)
+    categoryID = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    stock = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
