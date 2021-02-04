554fb18ce27de5c25c7a78da2307804cb51754c8
libasoj2 <joseph.libasora2@mail.dcu.ie>
2021-02-04 18:41:30 +0000

    Added address/payment/user models (Theky)

--- PCWare/PCStore/models.py
+++ PCWare/PCStore/models.py
@@ -14,3 +14,32 @@ class Product(models.Model):
     categoryID = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     stock = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Address(models.Model):
+    addressId = models.AutoField(primary_key=True)
+    line1 = models.CharField(max_length=100)
+    line2 = models.CharField(max_length=100)
+    line3 = models.CharField(max_length=100)
+    postcode = models.CharField(max_length=10)
+
+
+class Payment(models.Model):
+    paymentId = models.AutoField(primary_key=True)
+    holderName = models.CharField(max_length=200)
+    cardNum = models.IntegerField()
+    expireMonth = models.IntegerField()
+    expireYear = models.IntegerField()
+    cvv = models.IntegerField()
+    addressId = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+
+class User(models.Model):
+    userId = models.AutoField(primary_key=True)
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    contactNum = models.IntegerField()
+    addressId = models.ForeignKey(Address, on_delete=models.CASCADE)
+    paymentId = models.ForeignKey(Payment, on_delete=models.CASCADE)
+
