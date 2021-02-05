76a3b2dc208ebb98d85b05114b2e89fc839621b5
libasoj2 <joseph.libasora2@mail.dcu.ie>
2021-02-05 11:57:18 +0000

    Added SU + registered models

--- PCWare/PCStore/admin.py
+++ PCWare/PCStore/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductCategory)
+admin.site.register(Address)
+admin.site.register(Payment)
+admin.site.register(User)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Cart)
+admin.site.register(CartItem)
