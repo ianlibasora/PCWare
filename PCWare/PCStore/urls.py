93dccbfd90db8b03b977a527d1fe00bd4fb52acb
Theky Lien <the.lien3@mail.dcu.ie>
2021-02-05 12:52:07 +0000

    Added single products page

--- PCWare/PCStore/urls.py
+++ PCWare/PCStore/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', views.index, name="index-page"),
     path("register/", views.register, name="registration-page"),
-    path("all-products/", views.all_products, name="all-products")
+    path("all-products/", views.allProducts, name="all-products"),
+    path('singleproduct/<int:prodId>/', views.singleProduct, name='single-product')
 ]
--- PCWare/PCStore/views.py
+++ PCWare/PCStore/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 from django.http import HttpResponse
 
@@ -10,6 +10,11 @@ def index(request):
 def register(request):
     return render(request, "register.html")
 
-def all_products(request):
+def allProducts(request):
     all_p = Product.objects.all()
     return render(request, 'all_products.html', {'products': all_p})
+
+def singleProduct(request, prodId):
+    prod = get_object_or_404(Product, pk=prodId)
+    return render(request, 'single_product.html', {'product': prod})
+
