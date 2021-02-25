d9c81115b9932c58283e61a9ecc5eacfc85e55f4
libasoj2 <joseph.libasora2@mail.dcu.ie>
2021-02-25 17:38:49 +0000

    Basic registration page

--- PCWare/PCStore/forms.py
+++ PCWare/PCStore/forms.py
@@ -1,8 +1,23 @@
 from django.forms import ModelForm
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.db import transaction
 
 
 class ProductCategoryForm(ModelForm):
     class Meta:
         model = ProductCategory
         fields = {'categoryName', 'categoryDetails'}
+
+
+class UserSignUpForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.isAdmin = False
+        user.save()
+        return user
+
--- PCWare/PCStore/views.py
+++ PCWare/PCStore/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import CreateView
 from .models import *
 from .forms import *
 from django.http import HttpResponse
+from django.contrib.auth import login
+
 
 # Create your views here.
 
@@ -30,3 +33,17 @@ def productCategoryForm(request):
     else:
         form = ProductCategoryForm()
         return render(request, 'category_form.html', {'form': form})
+
+
+class UserSignUp(CreateView):
+    model = User
+    form_class = UserSignUpForm
+    template_name = "register.html"
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect("/")
