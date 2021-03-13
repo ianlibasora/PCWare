d7f8b300acb94837ee2a9fd6e081c05c0a8fc848
Theky Lien <the.lien3@mail.dcu.ie>
2021-03-13 13:16:47 +0000

    Added styles to form pages'

--- PCWare/PCStore/forms.py
+++ PCWare/PCStore/forms.py
@@ -15,18 +15,36 @@ class ProductCategoryForm(ModelForm):
         model = ProductCategory
         fields = ['categoryName', 'categoryDetails']
 
+        labels = {
+            'categoryName': 'Category Name',
+            'categoryDetails': 'Category Details'
+        }
+
 
 class ProductForm(ModelForm):
     category = CategoryChoiceField(queryset=ProductCategory.objects.all())
+
     class Meta:
         model = Product
         fields = ["productName", "productDetails", "category", "stock", "price", "picture"]
+
+        labels = {
+            'productName': 'Product Name',
+            'productDetails': 'Product Details'
+        }
 
 
 class UserSignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ["username", "first_name", "last_name", "email", "contactNum"]
+
+        labels = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email',
+            'contactNum': 'Contact Number'
+        }
 
     @transaction.atomic
     def save(self):
@@ -55,10 +73,23 @@ class AddressForm(ModelForm):
 
         fields = ["line1", "line2", "line3", "postcode"]
 
+        labels = {
+            'line1': 'Address-line 1',
+            'line2': 'Address-line 2',
+            'line3': 'Address-line 3',
+            'postcode': 'Postcode'
+        }
+
 
 class PaymentForm(ModelForm):
     class Meta:
         model = Payment
         fields = ["holderName", "cardNum", "expireMonth", "expireYear", "cvv"]
 
-    field_order = ["holderName", "cardNum", "expireMonth", "expireYear", "cvv"]
+        labels = {
+            'holderName': 'Card Owner Name',
+            'cardNum': 'Card Number',
+            'expireMonth': 'Expire Month',
+            'expireYear': 'Expire Year',
+            'cvv': 'CVV'
+        }
