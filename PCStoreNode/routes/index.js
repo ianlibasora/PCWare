5a292bf2191473717992dbb690e9aabb1c222e4c
libasoj2 <joseph.libasora2@mail.dcu.ie>
2021-04-16 10:39:02 +0100

    Added /all-products frontend

--- PCStoreNode/routes/index.js
+++ PCStoreNode/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'PCStore' });
+});
+
+router.get('/all-products', function(req, res, next) {
+  res.render('all-products', { title: 'PCSTore - All Products' });
 });
 
 module.exports = router;
